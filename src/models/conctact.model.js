e811ea3a058b560892eb3ee8321768c83f832d0d
hiepbc <chinhhiep996@gmail.com>
2019-07-28 15:45:35 +0700

    [1.0.0] define model Schemas

---
+++ src/models/conctact.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+  userId: String,
+  contactId: String,
+  status: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Number,
+    default: null
+  },
+  deleteAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
