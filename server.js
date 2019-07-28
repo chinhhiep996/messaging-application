21046b1d7f505923ea55fbeaeac2d4182219f3e7
hiepbc <chinhhiep996@gmail.com>
2019-07-28 09:44:27 +0700

    add BabelJS

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const hostname = 'localhost';
 const PORT = 8080;
 
 app.get('hello', (req, res) => {
-    res.send("<h1>Hello world !!!</h1>");
+  res.send("<h1>Hello world !!!</h1>");
 });
 
 app.listen(PORT, hostname, () => {
-    console.log('App running on port ' + hostname + ' ' + PORT);
+  console.log('App running on port http://' + hostname + ':' + PORT);
 });
